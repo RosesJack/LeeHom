b8634cd15e00cb32837250a6a008d108c6ae9b7c
unknown <318442028@qq.com>
2017-05-05 01:11:16 +0800

    add showing playing music UI

--- app/src/main/java/com/example/administrator/leehom/fragment/ThridFragment.java
+++ app/src/main/java/com/example/administrator/leehom/fragment/ThridFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.example.administrator.leehom.R;
 
@@ -16,11 +17,19 @@ import com.example.administrator.leehom.R;
 
 public class ThridFragment extends FragmentBase {
     public static final String TAG = "ThridFragment";
+    private View pre_bt;
+    private View next_bt;
+    private View play_or_pause;
+    private ProgressBar play_progress;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_thrid, container, false);
+        pre_bt = view.findViewById(R.id.pre_bt);
+        next_bt = view.findViewById(R.id.next_bt);
+        play_or_pause = view.findViewById(R.id.play_or_pause);
+        play_progress = (ProgressBar) view.findViewById(R.id.play_progress);
         return view;
     }
 
